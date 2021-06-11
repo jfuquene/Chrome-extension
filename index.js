00f5a95a0f89fea262f010131a01f4afcb871b9a
John Fuquene <jfuquene@email.wm.edu>
2021-06-11 19:55:08 -0400

    render leads

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}

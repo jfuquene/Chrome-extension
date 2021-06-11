e2397eb4502fafe608ae46a6b67ba1d1123f07d6
John Fuquene <jfuquene@email.wm.edu>
2021-06-11 19:53:13 -0400

    javascript variables

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
